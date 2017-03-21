592145e429edced9da8bdb33d6b8c5b7ced9c34b
yan.zhou <yan.zhou@beecloud.com>
2017-03-21 16:28:41 +0800

    111

--- src/main/java/com/beecloud/jmeter/action/ActionMapper.java
+++ src/main/java/com/beecloud/jmeter/action/ActionMapper.java
@@ -15,6 +15,14 @@ public class ActionMapper {
     static {
         /**开锁**/
         map.put(ApplicationID.UNLOCK.name() + 2,new UnLockMessage());
+        /**锁车**/
+        map.put(ApplicationID.LOCK.name() + 2,new LockMessage());
+        /**启动引擎**/
+        map.put(ApplicationID.START_CAR.name() + 2,new StartEngineMessage());
+        /**获取部件状态**/
+        map.put(ApplicationID.VEHICLE_COMPONENT_ACQUISITION.name() + 2,new StateFetchMessage());
+        /**远程开压缩机**/
+        map.put(ApplicationID.REMOTE_CONTROL.name() + 2,new ACRemoteControlMessage());
     }
 
     public static IMessage getMessage(String key){
